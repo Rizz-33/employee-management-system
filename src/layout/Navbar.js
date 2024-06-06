ea6d77ce806078d74fa82815dcb8b9baa48d8b3c
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-07 00:45:17 +0530

    feat: Navbar

---
+++ src/layout/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            E M S
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <button className="btn btn-outline-light">Add Emp</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;

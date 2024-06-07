1ae0753bd7396e1816fc0a5882e52eaf0eba5866
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-07 12:30:51 +0530

    refactor: Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import "./index.css"; // Import the Tailwind CSS file
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -15,38 +14,28 @@ const Home = () => {
   };
 
   return (
-    <div className="container mx-auto mt-12">
+    <div className="container">
       <div className="py-4">
-        <table className="min-w-full bg-white shadow-md rounded-lg">
-          <thead className="bg-gray-800 text-white">
+        <table className="table shadow">
+          <thead>
             <tr>
-              <th scope="col" className="py-2 px-4">
-                #
-              </th>
-              <th scope="col" className="py-2 px-4">
-                Name
-              </th>
-              <th scope="col" className="py-2 px-4">
-                Username
-              </th>
-              <th scope="col" className="py-2 px-4">
-                Email
-              </th>
-              <th scope="col" className="py-2 px-4">
-                Action
-              </th>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
             {users.map((user, index) => (
-              <tr key={index} className="border-b hover:bg-gray-100">
-                <th scope="row" className="py-2 px-4">
+              <tr>
+                <th scope="row" key={index}>
                   {index + 1}
                 </th>
-                <td className="py-2 px-4">{user.name}</td>
-                <td className="py-2 px-4">{user.username}</td>
-                <td className="py-2 px-4">{user.email}</td>
-                <td className="py-2 px-4 flex justify-center space-x-2">
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>
                   <button className="btn btn-outline-primary mx-2">View</button>
                   <button className="btn btn-primary mx-2">Edit</button>
                   <button className="btn btn-danger mx-2">Delete</button>

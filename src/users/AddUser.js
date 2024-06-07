6cd51735ceabf455a415e35d3efe132a8602f601
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-07 14:25:50 +0530

    feat: Post data

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,6 +1,11 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddUser = () => {
+
+  let navigate = useNavigate()
+
   const [user, setUser] = useState({
     name: "",
     username: "",
@@ -13,50 +18,58 @@ const AddUser = () => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8081/user", user);
+    navigate("/")
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-5 shadow">
           <h2 className="text-center m-4">Register Employee</h2>
-          <div className="mb-3">
-            <input
-              type="text"
-              placeholder="Name"
-              className="form-control"
-              id="name"
-              value={name}
-              onChange={onInputChange}
-              name="name"
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              placeholder="Username"
-              className="form-control"
-              id="username"
-              onChange={onInputChange}
-              value={username}
-              name="username"
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              placeholder="Email Address"
-              className="form-control"
-              id="email"
-              value={email}
-              onChange={onInputChange}
-              name="email"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary mx-3">
-            Submit
-          </button>
-          <button type="submit" className="btn btn-outline-danger">
-            Cancel
-          </button>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className="mb-3">
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control"
+                id="name"
+                value={name}
+                onChange={onInputChange}
+                name="name"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                placeholder="Username"
+                className="form-control"
+                id="username"
+                onChange={onInputChange}
+                value={username}
+                name="username"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                placeholder="Email Address"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={onInputChange}
+                name="email"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary mx-3">
+              Submit
+            </button>
+            <button type="submit" className="btn btn-outline-danger">
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </div>

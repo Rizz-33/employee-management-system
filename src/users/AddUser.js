c4e26965e7ca3bae68eec98dcb3df77039c43306
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-06-07 14:17:57 +0530

    refactor: User entering details

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddUser = () => {
-  return <div>AddUser Form</div>;
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { name, username, email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-5 shadow">
+          <h2 className="text-center m-4">Register Employee</h2>
+          <div className="mb-3">
+            <input
+              type="text"
+              placeholder="Name"
+              className="form-control"
+              id="name"
+              value={name}
+              onChange={onInputChange}
+              name="name"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              placeholder="Username"
+              className="form-control"
+              id="username"
+              onChange={onInputChange}
+              value={username}
+              name="username"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="text"
+              placeholder="Email Address"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={onInputChange}
+              name="email"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mx-3">
+            Submit
+          </button>
+          <button type="submit" className="btn btn-outline-danger">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddUser;
